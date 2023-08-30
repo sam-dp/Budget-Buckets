18b64695cf117219015caf63628c80a402b5b5bf
sam-dp <cloudysaga@gmail.com>
2023-08-30 01:40:40 -0700

    Starting bucket class

--- include/bucket.h
+++ include/bucket.h
@@ -1,4 +1,14 @@
 #ifndef BUDGET_BUCKETS_BUCKET_H
 #define BUDGET_BUCKETS_BUCKET_H
 
+class Bucket {
+private:
+    double size;
+    double value;
+
+public:
+
+
+};
+
 #endif //BUDGET_BUCKETS_BUCKET_H
