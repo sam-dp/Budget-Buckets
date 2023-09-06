2cd6c86f529563bf6c0620b6ea521bc9e3f8f5bf
sam-dp <cloudysaga@gmail.com>
2023-09-06 01:11:06 -0700

    Building CLI usage

--- src/main.cpp
+++ src/main.cpp
@@ -5,28 +5,82 @@
 #include "../include/Bucket.h"
 #include "../include/User.h"
 
+bool contains(int arr[], int size, int target) {
+    int left = 0;
+    int right = size - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            return true;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return false;
+}
 
 void printInfo(User myUser) {
-    std::cout << "---------------------------------" << std::endl;
-    std::cout << "-- User and Bucket Information --" << "\n---------------------------------" << "\nUsername: " << myUser.getUsername() <<  "\nMonthly Income: $" << myUser.getMonthlyIncome() << std::endl;
+    std::cout << "---------------------------------" << "\n"
+              << "-  User and Bucket Information  -" <<  "\n"
+              << "---------------------------------" <<  "\n"
+              << "Username: " << myUser.getUsername() <<  "\n"
+              << "Monthly Income: $" << myUser.getMonthlyIncome()
+              << std::endl;
 
     std::vector buckets = myUser.getAllBuckets();
 
     for(const Bucket& bucket : buckets) {
-        std::cout << "\nBucket Name: " << bucket.getName() << "\nBucket Size: $" << bucket.getSize() << "\nBucket Value: $" << bucket.getValue() << std::endl;
+        std::cout << "\n"
+                  << "Bucket Name: " << bucket.getName() <<  "\n"
+                  << "Bucket Size: $" << bucket.getSize() <<  "\n"
+                  << "Bucket Value: $" << bucket.getValue()
+                  << std::endl;
     }
 
     std::cout << "---------------------------------" << std::endl;
 
 }
 
+void choiceDialogue(const User& user) {
+    std::cout << "---------------------------------" << "\n"
+              << "-        Budget Buckets         -" <<  "\n"
+              << "---------------------------------" <<  "\n"
+              << "[1] View Bucket Information" <<  "\n"
+              << "[2] Add a Bucket" <<  "\n"
+              << "[3] Edit a Bucket" <<  "\n"
+              << "[4] Make a Payment on a bucket" <<  "\n\n"
+              << "Enter one of the following:";
+
+    short choice = -1;
+    int choices[] = {1, 2, 3, 4};
+    std::cin >> choice;
+    if(contains(choices, sizeof(choices), choice)) {
+        switch(choice) {
+            case 1:
+                printInfo(user);
+            case 2:
+
+            case 3:
+            case 4:
+            default:
+        }
+    }
+    
+}
 
 int main() {
     // Create account
     User myUser("Sam", 600.0);
     myUser.addBucket("Spending", 100);
+    myUser.addBucket("Savings", 300);
+    myUser.addBucket("Bills", 200);
 
-    printInfo(myUser);
+    //printInfo(myUser);
+    userDialogue();
+
+    return 0;
 
 }
 
